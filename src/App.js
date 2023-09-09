3112679794ba2ad220f4aafe3d2fcbf513783e3f
sethonne <20103261@usc.edu.ph>
2023-09-09 11:37:38 +0800

    edited Main component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Main from "./Main";
 import Header from "./Header";
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  });
+
   return (
     <div className="App">
       <Header />
