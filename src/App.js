e22a8e8f8a304057ea5d9014645a1630850214c1
sethonne <20103261@usc.edu.ph>
2023-09-09 11:43:07 +0800

    edited Main component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { useEffect, useReducer } from "react";
 import Main from "./Main";
 import Header from "./Header";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => console.error("Error"));
-  });
+  }, []);
 
   return (
     <div className="App">
